a8b52223a9e42e979b8cfd16e6de6d0de8c8eeb5
Benedict Tshaba <benedicttshaba@gmail.com>
2017-05-19 14:16:46 +0200

    Fixed the code - removed a lot of redundant 'self' keywords

--- tnt2_main.py
+++ tnt2_main.py
@@ -1,62 +1,88 @@
 #!/usr/bin/python
+
+
+__version__ = "0.0.1"
+__author__ = "Tshaba Phomolo Benedict"
+
 import Tkinter as tk
 import tkMessageBox
 import pickle
 import sched, time
 from tnt2lib import xor_crypt
 
-__version__ = "0.0.1"
-__author__ = "Tshaba Phomolo Benedict"
 notes_file = "notes.db"
 
-class MyApp(object):
+class Tnt2App(object):
 	def __init__(self,master):
 		self.master = master
 		self.master.title("TNT2")
 		self.master.geometry("600x400")
-		self.editor = Editor(self.master)
-		self.menubar = MenuBar(self.master)
-		self.editor.autoSave()
+		editor = Editor(self.master)
+		menubar = MenuBar(self.master)
+		editor.autoSave()
 		self.master.mainloop()
 
 class MenuBar(object):
 	def __init__(self,master):
 		self.master = master
-		self.menubar = tk.Menu(self.master)
-		self.filemenu = tk.Menu(self.menubar,tearoff=0)
-		self.filemenu.add_command(label="Exit",command=self.ExitCommand)
-		self.menubar.add_cascade(label="TnT2",menu=self.filemenu)
+		menubar = tk.Menu(self.master)
+		filemenu = tk.Menu(menubar,tearoff=0)
+		filemenu.add_command(label="Exit",command=self.exit_command)
+		menubar.add_cascade(label="TnT2",menu=filemenu)
+		menubar.add_separator()
+	
+		editmenu = tk.Menu(menubar, tearoff=0)
+		editmenu.add_command(label="Clear All", command=self.clear_command)
+		editmenu.add_separator()
+		editmenu.add_command(label="Prioritise", command=self.priori_command)
+		menubar.add_cascade(label="Edit",menu=editmenu)
+
+		helpmenu = tk.Menu(menubar, tearoff=0)
+		helpmenu.add_command(label="About", command=self.about_command)
+		menubar.add_cascade(label="Help", menu=helpmenu)
 
 		#display menu
-		self.master.config(menu=self.menubar)
+		self.master.config(menu=menubar)
+	
+	def about_command(self):
+		pass
 
-	def ExitCommand(self):
+	def remove_command(self):
+		pass
+
+	def clear_command(self):
+		pass	
+
+	def priori_command(self):
+		pass 
+
+	def exit_command(self):
 		if tkMessageBox.askyesno("Quit","Are you sure?"):
 			self.master.destroy()
 
 class Editor(object):
 	def __init__(self,master):
 		self.master = master
-		self.editor = tk.Frame(self.master)
-		self.notelistframe = tk.Frame(self.master)
+		editor = tk.Frame(self.master)
+		notelistframe = tk.Frame(self.master)
 
-		self.text = tk.Text(self.editor,bg="white",height=8, width=4)
-		self.text.pack(side=tk.BOTTOM,fill=tk.BOTH,expand=1)
+		text = tk.Text(editor,bg="white",height=8, width=4)
+		text.pack(side=tk.BOTTOM,fill=tk.BOTH,expand=1)
 
-		self.scroll1v = tk.Scrollbar(self.notelistframe,orient=tk.VERTICAL)
-		self.scroll2h = tk.Scrollbar(self.notelistframe,orient=tk.HORIZONTAL)
-		self.notelist = tk.Listbox(self.notelistframe,bg="grey",yscrollcommand=self.scroll1v.set,selectmode=tk.EXTENDED)
-		self.notelist = tk.Listbox(self.notelistframe,bg="grey",xscrollcommand=self.scroll2h.set,selectmode=tk.EXTENDED)
-		self.scroll1v.configure(command=self.notelist.yview)
-		self.scroll2h.configure(command=self.notelist.xview)
-		self.scroll2h.pack(side=tk.BOTTOM,fill=tk.X)
+		scroll1v = tk.Scrollbar(notelistframe,orient=tk.VERTICAL)
+		scroll2h = tk.Scrollbar(notelistframe,orient=tk.HORIZONTAL)
+		self.notelist = tk.Listbox(notelistframe,bg="grey",yscrollcommand=scroll1v.set,selectmode=tk.EXTENDED)
+		self.notelist = tk.Listbox(notelistframe,bg="grey",xscrollcommand=scroll2h.set,selectmode=tk.EXTENDED)
+		scroll1v.configure(command=self.notelist.yview)
+		scroll2h.configure(command=self.notelist.xview)
+		scroll2h.pack(side=tk.BOTTOM,fill=tk.X)
 		self.notelist.pack(side=tk.LEFT,fill=tk.BOTH,expand=1)
-		self.scroll1v.pack(side=tk.RIGHT,fill=tk.Y)
+		scroll1v.pack(side=tk.RIGHT,fill=tk.Y)
 
-		self.editor.pack(fill=tk.BOTH,side=tk.LEFT)
-		self.notelistframe.pack(fill=tk.BOTH,expand=1,side=tk.RIGHT)
-		self.buttons = Buttons(self.editor,self.text,self.notelist)
-		self.text.bind("<Return>",self.return_insert)
+		editor.pack(fill=tk.BOTH,side=tk.LEFT)
+		notelistframe.pack(fill=tk.BOTH,expand=1,side=tk.RIGHT)
+		self.buttons = Buttons(editor,text,self.notelist)
+		text.bind("<Return>",self.return_insert)
 		self.notelist.bind("<Double-Button-3>",self.delete_current)
 		self.getNotes()
 
@@ -113,4 +139,4 @@ class Buttons(object):
 
 if __name__ == '__main__':
 	root = tk.Tk()
-	myapp = MyApp(root)
+	myapp = Tnt2App(root)
