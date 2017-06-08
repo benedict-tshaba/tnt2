85156f1a89ca7f86df2f3c817fdc3e374502f134
Benedict Tshaba <benedicttshaba@gmail.com>
2017-06-08 15:51:50 +0200

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='TNT2',
+      version='1.0.1',
+      description='A simple note taking program',
+      author='Tshaba Phomolo B3n3dict',
+      author_email='benedicttshaba@gmail.com',
+      url='http://www.benedict.heliohost.org/',
+      packages=[],
+      license = "GPLv3",
+     )
