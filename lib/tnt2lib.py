b732df3490ea8af8b4632aff1250376833488725
Benedict Tshaba <benedicttshaba@gmail.com>
2017-06-09 09:28:16 +0200

    Fix Licence issues

--- lib/tnt2lib.py
+++ lib/tnt2lib.py
@@ -1,5 +1,22 @@
 #!/usr/bin/env python2
 
+# Tnt2 is a re-write of my previous program Tnt. It is a simple note-taking
+# program which I use to take class notes and write simple reminders.
+# Copyright (C) 2017  Tshaba Phomolo Benedict
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 def xor_crypt(s,mode):
 	"""returns a string of encoded text"""
 	sLen = len(s)
--- tnt/tnt2_main.py
+++ tnt/tnt2_main.py
@@ -1,5 +1,23 @@
 #!/usr/bin/python
 
+# Tnt2 is a re-write of my previous program Tnt. It is a simple note-taking
+# program which I use to take class notes and write simple reminders.
+# Copyright (C) 2017  Tshaba Phomolo Benedict
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+
 __version__ = "1.0.3"
 __author__ = "Tshaba Phomolo Benedict"
 
