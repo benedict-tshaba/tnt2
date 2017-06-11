3ee3120d6d6cecd4285dc6f1372728a756ef3614
Benedict Tshaba <benedicttshaba@gmail.com>
2017-06-11 17:54:46 +0200

    Update setup.py and remove config.py

--- config.py
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/python
-
-#To be used later when I will make some parts of the program configuarable
--- setup.py
+++ setup.py
@@ -2,14 +2,21 @@
 
 from distutils.core import setup
 
+with open('README.md') as f:
+    readme = f.read()
+
+with open('LICENSE') as f:
+    license = f.read()
+
 setup(name='tnt2',
       scripts=['bin/tnt2'],
       version='1.0.3',
       description='A simple note taking program',
+      long_description = readme,
       author='Tshaba Phomolo Benedict',
       author_email='benedicttshaba@gmail.com',
       url='http://www.benedict.heliohost.org/',
       packages=['lib','tnt'],
       include_package_data = True,
-      license = "GPLv3",
+      license = license,
      )
