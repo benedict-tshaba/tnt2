fc84fdf43c6decf99b92d5e77ab74e311393bd9d
Tshaba Phomolo Benedict <benedicttshaba@gmail.com>
2017-07-01 10:26:04 +0200

    Update to version 1.2.0

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open('LICENSE') as f:
 
 setup(name='tnt2',
       scripts=['bin/tnt2'],
-      version='1.1.5',
+      version='1.2.0',
       description='A simple note taking program',
       long_description = readme,
       author='Tshaba Phomolo Benedict',
--- tnt/tnt2_main.py
+++ tnt/tnt2_main.py
@@ -18,7 +18,7 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 
-__version__ = "1.1.5"
+__version__ = "1.2.0"
 __author__ = "Tshaba Phomolo Benedict"
 
 import Tkinter as tk
